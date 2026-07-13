689b7f4d5b57505e1a5a5726bc9d38ad30a4b541
MyAtul <atuly2409@gmail.com>
2026-07-13 19:41:31 +0530

    Refactoring frontend for deployment

--- frontend/src/app/providers/AppProviders.tsx
+++ frontend/src/app/providers/AppProviders.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import AuthProvider from '../../modules/auth/context/AuthContext'
 
 type AppProvidersProps = {
