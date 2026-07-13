9ef23178f87f215b7bf3f072a739e4444b65a8af
MyAtul <atuly2409@gmail.com>
2026-07-13 20:29:59 +0530

    Configure backend for deployment

--- backend/src/main/java/com/devhub/backend/modules/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/devhub/backend/modules/auth/config/SecurityConfig.java
@@ -67,8 +67,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/auth/register",
                                 "/api/v1/auth/login",
-                                "/api/v1/auth/refresh",
-                                "/api/v1/modules/authentication/download/**"
+                                "/api/v1/auth/refresh"
                         ).permitAll()
 
                         .anyRequest().authenticated()
